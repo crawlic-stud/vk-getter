3877662b3ec78ec78d607e515e72518bfbcfd140
crawlic-stud <nikitosik0726@gmail.com>
2022-09-28 23:54:07 +0300

    initial

--- push.sh
+++ push.sh
@@ -1,3 +1,9 @@
+if [ "$1" = "" ]
+then
+  echo "Please provide commit message"
+  exit 0
+fi
+
 git add .
-git commit -m "[UPDATE] version $1"
-git push origin master
+git commit -m "$1"
+git push origin dev
